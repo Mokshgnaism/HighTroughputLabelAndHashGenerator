5fe8a198b5eea52b8216fad7670252cd1fe53e62
MokshgnaIsm <gorantlamokshgna980@gmail.com>
2026-02-19 13:35:44 +0530

    added the comment for Success and benchmarks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -50,7 +50,6 @@ public class Main {
         int chunk = (noOfPallets + palletWorkers - 1) / palletWorkers;
         ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
-
         for (int i = 0; i < palletWorkers; i++) {
             int startIdx = i * chunk;
             int endIdx = Math.min(startIdx + chunk, noOfPallets);
@@ -155,3 +154,29 @@ public class Main {
         System.out.println("Total execution time: "+(end-start)/1000000);
     }
 }
+
+// the pipeline is quite simple but yet effective .
+// first off all it is not a pipeline in the regular language
+/*
+*           THIS PROJECT DOES THE FOLLOWING :
+*   -> generates PALLET IDS, hashes them which are the base containers for products ,
+*   -> generates cartonIds for the cartons and these cartons have a very specific role these are the first box inside the pallets
+*   -> generates the unitids and hashes them as well these unit ids are the ids for products  which are inside the cartons
+*   KEY CHARACHERSTIC:
+*               -> it follows a complete streaming architecture . no blocking stage until and unless completely necessary
+*               -> the architecture is as follows
+*                       -> there are two functions
+*                               -> one is generateAndInsertPalletIds which takes a pipedoutputstream . generates the ids and writes to the pos
+*                               -> another is db inserter which has its own pipedinput stream whihc is connected to the pos from the above function .
+*                               -> so as the labels are being generated they are being streamed to copy using a csv without lag
+*                               -> now comes the next state . we need the ssics of pallets to generate the serialids of cartons
+*                               -> so as we generate the pallet ids we insert them into a blocking queue
+*                               -> then we spin up some other workers(to genrarte and insert cartons) which consume from the above blocking queue and do the same above two functions.
+*                               -> we use poison placing to end the producer consumer loop.
+*               -> benchmarking gave a superb  resultt of 8 seconds for a huge 5.51Million labels hashed and inserted into postgres
+*               -> original SLA was 10k labels under 5 seconds .
+*               -> will try to optimise more to get there .
+*               -> there are some tradeoffs like Mac cloning(warm start for hashing function which requires cloning) or without warm start completely hash the payload .
+*               -> and is using binary protocol that necessary when we are that close to the SLA is thequesiton and some byte array copys need to be checked in the Labelgenerator Class
+*               -> they are the optimisations remaining .
+* */
